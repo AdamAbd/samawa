bb85bfec5fdccab42bea00b8241818463e32f1b9
AdamAbd <adamabd14113@gmail.com>
2024-10-24 09:57:54 +0700

    remove layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+
+	import Header from './Header.svelte';
 </script>
 
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<div>
+	<Header />
+
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+</div>
