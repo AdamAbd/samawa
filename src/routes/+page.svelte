85afdfa17db1853ebb0888d28ec31771dd947102
AdamAbd <adamabd14113@gmail.com>
2024-10-26 23:31:45 +0700

    add reason section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,14 @@
 	import Crown from '$lib/images/crown.svg';
 	import Location from '$lib/images/location.svg';
 	import House from '$lib/images/house.svg';
+	import Like from '$lib/images/like.svg';
 </script>
 
 <div>
 	<Header />
 
 	<div class="flex flex-col">
-		<div>
+		<section>
 			<div class="flex items-center px-[50px] w-[1000px] h-[480px] bg-red-50 rounded-[30px]">
 				<div class="flex flex-col gap-5 p-5 w-[305px] h-fit bg-white rounded-[20px]">
 					<div
@@ -41,7 +42,54 @@
 					</button>
 				</div>
 			</div>
-		</div>
+		</section>
+
+		<section id="reason" class="container mx-auto my-[70px] flex flex-col items-center gap-[30px]">
+			<h2 class="text-3xl font-bold text-center leading-10">
+				Alasan Mereka Memilih <br /> Wedding Package Samawa
+			</h2>
+			<div class="inline-flex gap-10">
+				<div
+					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+				>
+					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+					<div class="flex flex-col gap-2">
+						<h3 class="text-xl font-bold">Dipercaya Sejak 1970</h3>
+						<span
+							>Kami telah melayani dengan kepercayaan dan dedikasi sejak tahun 1970, memastikan
+							setiap momen pernikahan sempurna.</span
+						>
+					</div>
+					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
+				</div>
+				<div
+					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+				>
+					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+					<div class="flex flex-col gap-2">
+						<h3 class="text-xl font-bold">Boleh Refund 100%</h3>
+						<span
+							>Kepuasan Anda adalah prioritas kami. Nikmati jaminan pengembalian dana 100% tanpa
+							syarat.</span
+						>
+					</div>
+					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
+				</div>
+				<div
+					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+				>
+					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+					<div class="flex flex-col gap-2">
+						<h3 class="text-xl font-bold">Top Organizers</h3>
+						<span
+							>Dengan tim terbaik, kami siap mewujudkan pernikahan impian Anda dengan layanan
+							profesional dan berpengalaman.</span
+						>
+					</div>
+					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
+				</div>
+			</div>
+		</section>
 	</div>
 
 	<h1 class="text-3xl font-bold underline">Hello world!</h1>
