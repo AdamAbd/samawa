9e5ff5607fe4cf414dfa39fdb8e9f8226ab0cad3
AdamAbd <adamabd14113@gmail.com>
2024-10-27 23:31:44 +0700

    add package section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    container: {
+      padding: {
+        xl: '4rem',
+      },
+    },
     extend: {
       fontFamily: { "poppins": ["Poppins", "sans-serif"] },
     }
