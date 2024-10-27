6f540d2388a8732a67f1cb207b317944b12b7fd3
AdamAbd <adamabd14113@gmail.com>
2024-10-27 12:18:15 +0700

    add review section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,9 @@
 	import Location from '$lib/images/location.svg';
 	import House from '$lib/images/house.svg';
 	import Like from '$lib/images/like.svg';
+	import london from '$lib/images/london.png';
+	import star from '$lib/images/star.svg';
+	import user from '$lib/images/user.png';
 </script>
 
 <div>
@@ -90,7 +93,267 @@
 				</div>
 			</div>
 		</section>
-	</div>
 
-	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+		<section class="py-[70px] w-full bg-[#F7F7F9]">
+			<div class="container mx-auto flex flex-col gap-y-7">
+				<div class="inline-flex items-center justify-between">
+					<h2 class="text-3xl font-bold leading-10">
+						Browse Packages <br /> City Recomendation
+					</h2>
+					<button class="px-5 py-3.5 h-fit border border-black rounded-full">
+						<span class="font-semibold">Explore All</span>
+					</button>
+				</div>
+				<div class="grid grid-cols-3 gap-x-7 gap-y-6">
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+						<img src={london} alt="" />
+						<div class="flex flex-col">
+							<span class="text-xl font-bold">London</span>
+							<span>183,498 Packages</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="review" class="my-[70px] flex flex-col gap-[30px]">
+			<div class="container mx-auto inline-flex items-center justify-between">
+				<h2 class="text-3xl font-bold leading-10">
+					Happy Stories of <br />
+					Our Wedding
+				</h2>
+			</div>
+
+			<div class="marquee">
+				<ul class="marquee__content">
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+				</ul>
+				<!-- Mirrors the content above -->
+				<ul class="marquee__content" aria-hidden="true">
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+					<div class="flex flex-col gap-5 p-[30px] w-[340px] border rounded-[30px]">
+						<div class="inline-flex gap-1">
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+							<img class="h-5 aspect-square" src={star} alt="Star Location" />
+						</div>
+						<p class="leading-7">
+							Keren banget Samawa, gue bisa nikah dengan pengalaman bagus dengan harga terjangkau!
+							Good.
+						</p>
+						<div class="inline-flex gap-5 items-center">
+							<img class="h-[70px] aspect-square" src={user} alt="User Location" />
+							<div class="flex flex-col">
+								<b>Andriana Putri</b>
+								<span>Programmer</span>
+							</div>
+						</div>
+					</div>
+				</ul>
+			</div>
+		</section>
+	</div>
 </div>
+
+<style>
+	.marquee {
+		--gap: 2.5rem;
+		display: flex;
+		overflow: hidden;
+		user-select: none;
+		gap: var(--gap);
+	}
+
+	.marquee__content {
+		flex-shrink: 0;
+		display: flex;
+		justify-content: space-around;
+		min-width: 100%;
+		gap: var(--gap);
+		animation: scroll 10s linear infinite;
+	}
+
+	@keyframes scroll {
+		from {
+			transform: translateX(0);
+		}
+		to {
+			transform: translateX(calc(-100% - var(--gap)));
+		}
+	}
+</style>
