4c15c2984722c748f5968dc0fa555264575dd779
AdamAbd <adamabd14113@gmail.com>
2024-11-12 10:07:08 +0700

    add carousel slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
-<script>
+<script lang="ts">
+	import { page } from '$app/stores';
+
 	import '../app.css';
 	// import 'remixicon/fonts/remixicon.css';
 
@@ -13,90 +15,80 @@
 
 	import WeedingPackage from '$lib/images/package.png';
 	import Footer from './Footer.svelte';
+	import Carousel from '$lib/components/Carousel.svelte';
+
+	let city = $page.url.searchParams.get('city');
+
+	const capitalizeFirstLetter = (val: string) => {
+		return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+	};
 </script>
 
 <div>
 	<Header />
 
 	<div class="flex flex-col mt-[114px]">
-		<section>
-			<div class="flex items-center px-[50px] w-[1000px] h-[480px] bg-red-50 rounded-[30px]">
-				<div class="flex flex-col gap-5 p-5 w-[305px] h-fit bg-white rounded-[20px]">
-					<div
-						class="inline-flex items-center space-x-2 px-3.5 py-1.5 w-fit bg-[#5C2D2D] rounded-full"
-					>
-						<img class="h-[18px] aspect-square" src={Crown} alt="Icon Crow" />
-						<span class="text-sm text-white font-bold">POPULAR</span>
-					</div>
-					<div class="flex flex-col gap-2">
-						<span class="text-[28px] leading-[38px] font-bold"
-							>Nikah Muda Abadi Pantai Bali Nusa</span
-						>
-						<span class="text-lg text-[#FF48B6] font-semibold">Rp 198.493.000</span>
-					</div>
-					<div class="inline-flex items-center gap-3.5">
-						<div class="inline-flex gap-1.5 items-center">
-							<img class="h-5 aspect-square" src={Location} alt="Icon Location" />
-							<span class="text-sm font-bold">Luxemborgio</span>
-						</div>
-						<div class="inline-flex gap-1.5 items-center">
-							<img class="h-5 aspect-square" src={House} alt="Icon Location" />
-							<span class="text-sm font-bold">Tentram</span>
-						</div>
-					</div>
-					<button class="px-5 py-3.5 w-full rounded-full bg-[#FF48B6]">
-						<span class="font-semibold text-white">View Package</span>
-					</button>
-				</div>
+		{#if city}
+			<div class="container mx-auto flex flex-row justify-between mb-[30px]">
+				<h1 class="text-[40px] font-bold">
+					Wedding Packages <br /> in {capitalizeFirstLetter(city)} City
+				</h1>
 			</div>
-		</section>
+		{/if}
 
-		<section id="reason" class="container mx-auto my-[70px] flex flex-col items-center gap-[30px]">
-			<h2 class="text-3xl font-bold text-center leading-10">
-				Alasan Mereka Memilih <br /> Wedding Package Samawa
-			</h2>
-			<div class="inline-flex gap-10">
-				<div
-					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
-				>
-					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
-					<div class="flex flex-col gap-2">
-						<h3 class="text-xl font-bold">Dipercaya Sejak 1970</h3>
-						<span
-							>Kami telah melayani dengan kepercayaan dan dedikasi sejak tahun 1970, memastikan
-							setiap momen pernikahan sempurna.</span
-						>
+		<Carousel />
+
+		{#if !city}
+			<section
+				id="reason"
+				class="container mx-auto my-[70px] flex flex-col items-center gap-[30px]"
+			>
+				<h2 class="text-3xl font-bold text-center leading-10">
+					Alasan Mereka Memilih <br /> Wedding Package Samawa
+				</h2>
+				<div class="inline-flex gap-10">
+					<div
+						class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+					>
+						<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+						<div class="flex flex-col gap-2">
+							<h3 class="text-xl font-bold">Dipercaya Sejak 1970</h3>
+							<span
+								>Kami telah melayani dengan kepercayaan dan dedikasi sejak tahun 1970, memastikan
+								setiap momen pernikahan sempurna.</span
+							>
+						</div>
+						<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
 					</div>
-					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
-				</div>
-				<div
-					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
-				>
-					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
-					<div class="flex flex-col gap-2">
-						<h3 class="text-xl font-bold">Boleh Refund 100%</h3>
-						<span
-							>Kepuasan Anda adalah prioritas kami. Nikmati jaminan pengembalian dana 100% tanpa
-							syarat.</span
-						>
+					<div
+						class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+					>
+						<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+						<div class="flex flex-col gap-2">
+							<h3 class="text-xl font-bold">Boleh Refund 100%</h3>
+							<span
+								>Kepuasan Anda adalah prioritas kami. Nikmati jaminan pengembalian dana 100% tanpa
+								syarat.</span
+							>
+						</div>
+						<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
 					</div>
-					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
-				</div>
-				<div
-					class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
-				>
-					<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
-					<div class="flex flex-col gap-2">
-						<h3 class="text-xl font-bold">Top Organizers</h3>
-						<span
-							>Dengan tim terbaik, kami siap mewujudkan pernikahan impian Anda dengan layanan
-							profesional dan berpengalaman.</span
-						>
+					<div
+						class="flex flex-col flex-1 items-start gap-5 p-[30px] min-w-[340px] border rounded-[30px]"
+					>
+						<img class="h-[50px] aspect-square" src={Like} alt="Icon Like" />
+						<div class="flex flex-col gap-2">
+							<h3 class="text-xl font-bold">Top Organizers</h3>
+							<span
+								>Dengan tim terbaik, kami siap mewujudkan pernikahan impian Anda dengan layanan
+								profesional dan berpengalaman.</span
+							>
+						</div>
+						<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
 					</div>
-					<a href="/" class="text-[#FF48B6] font-semibold underline">Learn More</a>
 				</div>
-			</div>
-		</section>
+			</section>
+		{/if}
 
 		<section
 			id="packages"
@@ -181,62 +173,64 @@
 			</div>
 		</section>
 
-		<section class="py-[70px] w-full bg-[#F7F7F9]">
-			<div class="container mx-auto flex flex-col gap-y-7">
-				<div class="inline-flex items-center justify-between">
-					<h2 class="text-3xl font-bold leading-10">
-						Browse Packages <br /> City Recomendation
-					</h2>
-					<button class="px-5 py-3.5 h-fit border border-black rounded-full">
-						<span class="font-semibold">Explore All</span>
-					</button>
+		{#if !city}
+			<section class="py-[70px] w-full bg-[#F7F7F9]">
+				<div class="container mx-auto flex flex-col gap-y-7">
+					<div class="inline-flex items-center justify-between">
+						<h2 class="text-3xl font-bold leading-10">
+							Browse Packages <br /> City Recomendation
+						</h2>
+						<button class="px-5 py-3.5 h-fit border border-black rounded-full">
+							<span class="font-semibold">Explore All</span>
+						</button>
+					</div>
+					<div class="grid grid-cols-3 gap-x-7 gap-y-6">
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+						<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
+							<img src={london} alt="" />
+							<div class="flex flex-col">
+								<span class="text-xl font-bold">London</span>
+								<span>183,498 Packages</span>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div class="grid grid-cols-3 gap-x-7 gap-y-6">
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-					<div class="inline-flex items-center gap-5 p-5 rounded-[30px] bg-white">
-						<img src={london} alt="" />
-						<div class="flex flex-col">
-							<span class="text-xl font-bold">London</span>
-							<span>183,498 Packages</span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+			</section>
+		{/if}
 
 		<section id="review" class="my-[70px] flex flex-col gap-[30px]">
 			<div class="container mx-auto inline-flex items-center justify-between">
@@ -434,7 +428,7 @@
 		justify-content: space-around;
 		min-width: 100%;
 		gap: var(--gap);
-		animation: scroll 10s linear infinite;
+		animation: scroll 20s linear infinite;
 	}
 
 	@keyframes scroll {
